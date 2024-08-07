f972bbcafacb9a4ad3af61cc1cf14acd0bb53973
Cameron Foreman <cameronforeman414@gmail.com>
2024-08-07 17:32:37 -0500

    Use omnisharp instead of csharp_ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'pylsp', 'csharp_ls'},
+  ensure_installed = {'pylsp', 'omnisharp'},
   handlers = {
     lsp_zero.default_setup,
     lua_ls = function()
