2fc592b3a20611c853878eddebf4a4326b85b71e
Cameron Foreman <cameronforeman414@gmail.com>
2025-02-17 15:19:10 -0600

    Add rendermarkdown

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "help", "c_sharp", "json", "python", "sql", "toml", "xml", "yaml", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "help", "c_sharp", "json", "python", "sql", "toml", "xml", "yaml", "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
